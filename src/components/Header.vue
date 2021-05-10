9e9c03299481eb5ea2b8aba7bb1c5c320ba816db
MatteoLiberati <matteoliberati@hotmail.com>
2021-05-10 11:35:19 +0200

    inizializzazione

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <slot></slot>
+    <div class="input">
+      <input type="text" />
+      <button>Cerca</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style></style>
